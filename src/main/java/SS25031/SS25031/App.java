e863284e0c934027ad92ddd332b30e00072a1c88
katherinneSantos <SS25031@ues.edu.sv>
2025-05-10 15:50:13 -0600

    Replace

---
+++ src/main/java/SS25031/SS25031/App.java
@@ -0,0 +1,17 @@
+package SS25031.SS25031;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello(){
+		return "Hello World!!";
+	}
+    public static void main( String[] args )
+    {
+        App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/SS25031/SS25031/AppTest.java
@@ -0,0 +1,12 @@
+	package SS25031.SS25031;
+	
+	import org.junit.jupiter.api.Test;
+	import static org.junit.jupiter.api.Assertions.assertTrue;
+
+	
+public class AppTest{
+	@Test
+	public void testHello() {
+		assertTrue(true);
+	}
+}
