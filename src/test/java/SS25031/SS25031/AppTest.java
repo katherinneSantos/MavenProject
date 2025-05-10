d85efe9fc757c82edc2178b252534c1809ae266b
katherinneSantos <SS25031@ues.edu.sv>
2025-05-10 16:38:18 -0600

    fixing

--- src/test/java/SS25031/SS25031/AppTest.java
+++ src/test/java/SS25031/SS25031/AppTest.java
@@ -1,7 +1,7 @@
 	package SS25031.SS25031;
 	
 	import org.junit.jupiter.api.Test;
-	import static org.junit.jupiter.api.Assertions.assertTrue;
+	import static org.junit.jupiter.api.Assertions.*;
 
 	
 public class AppTest{
